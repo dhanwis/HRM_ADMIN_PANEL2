9be5d2bfb4ea1732114d9e01281f144dce989cc2
Muhammed-shamal <muhammedshamalpv@gmail.com>
2024-07-19 10:05:49 +0530

    employee data table creation + urls updated with Image url

--- src/pages/HR-Section/Registration.js
+++ src/pages/HR-Section/Registration.js
@@ -10,12 +10,10 @@ import {
   DatePicker,
 } from "antd";
 
-import { format } from "date-fns";
-
 import "antd/dist/antd.css";
 import vector from "../../assets/images/vectorhr.png";
 import axios from "axios";
-import { baseUrl } from "../../url";
+import { baseUrl, baseUrlImg } from "../../url";
 
 const EmployeeRegistrationForm = () => {
   const [employees, setEmployees] = useState([]);
@@ -51,6 +49,20 @@ const EmployeeRegistrationForm = () => {
     setIsModalVisible(false);
   };
 
+  useEffect(() => {
+    console.log(employees.length);
+    if (employees.length === 0) {
+      fetch(`${baseUrl}/Teamlead/`, {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setEmployees(data);
+          console.log(data);
+        });
+    }
+  }, [employees]);
+
   const viewEmployee = (id) => {
     const employee = employees.find((emp) => emp.id === id);
     setSelectedEmployee(employee);
@@ -81,15 +93,37 @@ const EmployeeRegistrationForm = () => {
       key: "id",
     },
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      title: "Uname",
+      dataIndex: "username",
+      key: "uname",
     },
     {
       title: "Role",
       dataIndex: "role",
       key: "role",
     },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "PhoneNumber",
+      dataIndex: "phone_number",
+      key: "phone_number",
+    },
+    {
+      title: "Department",
+      dataIndex: "department",
+      key: "department",
+    },
+
+    {
+      title: "Experience",
+      dataIndex: "experience",
+      key: "experience",
+    },
+
     {
       title: "Actions",
       key: "actions",
@@ -219,7 +253,7 @@ const EmployeeForm = ({ visible, onCancel, onCreate }) => {
       console.log("response", response);
 
       if (response.status === 201) {
-        onCreate(response.data.user);
+        onCreate(response.data);
         message.success("User created successfully!");
       } else {
         message.error("Failed to create user. Please try again later.");
@@ -255,161 +289,6 @@ const EmployeeForm = ({ visible, onCancel, onCreate }) => {
       width={1000}
       maskClosable={false}
     >
-      {/* <Form form={form} layout="vertical" name="employee_form">
-        <Form.Item
-          name="role"
-          label="Role"
-          rules={[{ required: true, message: "Please select a role!" }]}
-        >
-          <Select>
-            <Select.Option value="Teamlead">Teamlead</Select.Option>
-            <Select.Option value="Staff">Staff</Select.Option>
-            <Select.Option value="Frontoffice">Frontoffice</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          name="name"
-          label="Name"
-          rules={[
-            { message: "Please input the name!" },
-            { pattern: /^[A-Za-z\s]+$/, message: "Please enter a valid name." },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="username"
-          label="User Name"
-          rules={[
-            { required: true, message: "Please input the username!" },
-            {
-              pattern: /^[A-Za-z\s]+$/,
-              message: "Please enter a valid username.",
-            },
-          ]}
-        >
-          <Input autoComplete="user_name" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          label="Password"
-          rules={[
-            {
-              required: true,
-              message: "Please input the password!",
-              pattern:
-                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
-              message: "Please enter a valid password.",
-            },
-          ]}
-        >
-          <Input.Password autoComplete="new-password" />
-        </Form.Item>
-
-        <Form.Item
-          name="image"
-          label="Photo"
-          rules={[{ message: "Please upload a photo!" }]}
-        >
-          <Input type="file" />
-        </Form.Item>
-        <Form.Item
-          name="email"
-          label="Email"
-          rules={[
-            {
-              required: true,
-              message: "Please input the email!",
-              type: "email",
-            },
-          ]}
-        >
-          <Input autoComplete="email" />
-        </Form.Item>
-        <Form.Item
-          name="phone"
-          label="Phone"
-          rules={[
-            { message: "Please input the phone number!" },
-            {
-              pattern: /^\d{10}$/,
-              message: "Phone number should contain 10 digits.",
-            },
-          ]}
-        >
-          <Input autoComplete="tel" />
-        </Form.Item>
-        <Form.Item
-          name="maritalStatus"
-          label="Marital Status"
-          rules={[{ message: "Please select marital status!" }]}
-        >
-          <Select>
-            <Select.Option value="single">Single</Select.Option>
-            <Select.Option value="married">Married</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          name="address"
-          label="Address"
-          rules={[{ message: "Please input the address!" }]}
-        >
-          <Input.TextArea autoComplete="street-address" />
-        </Form.Item>
-        <Form.Item
-          name="city"
-          label="City"
-          rules={[{ message: "Please input the city!" }]}
-        >
-          <Input autoComplete="address-level2" />
-        </Form.Item>
-        <Form.Item
-          name="state"
-          label="State"
-          rules={[{ message: "Please input the state!" }]}
-        >
-          <Input autoComplete="address-level1" />
-        </Form.Item>
-        <Form.Item
-          name="country"
-          label="Country"
-          rules={[{ message: "Please input the country!" }]}
-        >
-          <Input autoComplete="country" />
-        </Form.Item>
-        <Form.Item
-          name="pincode"
-          label="Pin Code"
-          rules={[
-            { message: "Please input the pin code!" },
-            { pattern: /^\d{6}$/, message: "Pincode should contain 6 digits." },
-          ]}
-        >
-          <Input autoComplete="postal-code" />
-        </Form.Item>
-        <Form.Item
-          name="qualification"
-          label="Qualification"
-          rules={[{ message: "Please input the qualification!" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="experience"
-          label="Experience"
-          rules={[{ message: "Please input the experience!" }]}
-        >
-          <Select>
-            <Select.Option value="0 year">0 year</Select.Option>
-            <Select.Option value="1 year">1 year</Select.Option>
-            <Select.Option value="2 years">2 years</Select.Option>
-            <Select.Option value="more than 2 years">
-              More than 2 years
-            </Select.Option>
-          </Select>
-        </Form.Item>
-      </Form> */}
-
       <Form form={form} layout="vertical" name="employee_form">
         <Form.Item
           name="role"
@@ -605,47 +484,29 @@ const EmployeeDetails = ({ employee, onClose }) => {
         <strong>ID:</strong> {employee.id}
       </p>
       <p>
-        <strong>Name:</strong> {employee.name}
-      </p>
-      <p>
         <strong>Username:</strong> {employee.username}
       </p>
+
       <p>
-        <strong>Password:</strong> {employee.password}
+        <strong>Employee Image:</strong>{" "}
+        <img src={`${baseUrlImg}${employee.image}`} alt="EmployeeImg" />
       </p>
-      <p>
-        <strong>Photo:</strong> {employee.photo}
-      </p>
+
       <p>
         <strong>Email:</strong> {employee.email}
       </p>
       <p>
-        <strong>Phone:</strong> {employee.phone}
+        <strong>Phone:</strong> +91 {employee.phone_number}
       </p>
-      <p>
-        <strong>Marital Status:</strong> {employee.maritalStatus}
-      </p>
+
       <p>
         <strong>Address:</strong> {employee.address}
       </p>
-      <p>
-        <strong>City:</strong> {employee.city}
-      </p>
-      <p>
-        <strong>State:</strong> {employee.state}
-      </p>
-      <p>
-        <strong>Country:</strong> {employee.country}
-      </p>
-      <p>
-        <strong>Pin code:</strong> {employee.pincode}
-      </p>
+
       <p>
         <strong>Role:</strong> {employee.role}
       </p>
-      <p>
-        <strong>Qualification:</strong> {employee.qualification}
-      </p>
+
       <p>
         <strong>Experience:</strong> {employee.experience}
       </p>
--- src/url.js
+++ src/url.js
@@ -1,2 +1,3 @@
 export const baseUrl = "http://192.168.1.6:8000/authapp";
 export const baseUrlHr = "http://192.168.1.6:8000/hr";
+export const baseUrlImg = "http://192.168.1.6:8000";
